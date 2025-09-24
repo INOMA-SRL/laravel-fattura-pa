6f1dcaa980d36e416f1c3003d02a04f417ba82bd
Simone-Inoma <simone.cerruti@inoma.it>
2025-09-24 16:18:04 +0200

    chore: rector config

--- rector.php
+++ rector.php
@@ -10,18 +10,17 @@ return RectorConfig::configure()
         __DIR__.'/src',
     ])
     ->withPhpSets(php84: true)
-// ->withSets([
-//     LaravelSetList::LARAVEL_CODE_QUALITY,
-//     LaravelSetList::LARAVEL_COLLECTION,
-//     LaravelSetList::LARAVEL_120,
-// ])
+    ->withSets([
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_120,
+        LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+    ])
     ->withPreparedSets(
         deadCode: true,
-        // codeQuality: true,
         typeDeclarations: true,
-        // privatization: true,
-        // instanceOf: true,
-        // earlyReturn: true,
-        // strictBooleans: true,
+        privatization: true,
+        earlyReturn: true,
         rectorPreset: true,
+        carbon: true
     );
